f7a52f5da017a4fadbe2fc1be678fe46568e2c5b
sndr26 <sndr26ms@gmail.com>
2021-07-07 21:08:51 +0530

    initialize array datastructure

---
+++ src/com/sndr/impl/Arrays.java
@@ -0,0 +1,24 @@
+package com.sndr.impl;
+
+import com.sndr.service.ArrayService;
+public class Arrays implements ArrayService {
+
+	@Override
+	public boolean add(Object element) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(int index) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
